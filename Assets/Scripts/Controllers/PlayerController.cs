3cf496c1f8ed4527e78a1cb9cccb15053e807a8e
Anthony Capobianchi <anthony.capobianchi@gmail.com>
2015-10-28 16:09:42 -0400

    started recursive pushback

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -10,24 +10,28 @@ public class PlayerController : MonoBehaviour {
 	private Vector3 moveDirection = Vector3.zero;
 	private float gravity;
 
+	private CollisionSphere collisionSphere;
+
 	private GroundController groundController;
 
+	public float CollisionOffset;
 	public float Radius = 0.5f;
 	public LayerMask Walkable;
 
 	public Vector3 Up{ get{ return transform.up; } }
 	public Vector3 Down{ get{ return -transform.up; } }
 
+	private const int MaxPushbackIterations = 2;
+
 	void Awake () {
 		
 		groundController = new GroundController( Walkable, this );
-
-		currentlyClampedTo = null;
 
 	}
 
 	void Start () {
 		gravity = WorldProperties.Instance.Gravity;
+		collisionSphere = new CollisionSphere( transform.position, CollisionOffset );
 	}
 	
 	void DoUpdate () {
@@ -46,11 +50,36 @@ public class PlayerController : MonoBehaviour {
 
 	private void calculatePushbackPhase()
 	{
+		recursivePushback();
+	}
+
+	private void recursivePushback()
+	{
+		recursivePushback( 0, MaxPushbackIterations );
+	}
+
+	private void recursivePushback( int depth, int maxDepth )
+	{
 		bool contact = false;
-		foreach( Collider c in Physics.OverlapSphere( Transform.position, Radius, Walkable ) )
+		foreach( Collider c in Physics.OverlapSphere( transform.position, Radius, Walkable ) )
 		{
 			if( c.isTrigger )
 				continue;
+
+			Vector3 position = collisionSphere.Position;
+			Vector3 contactPoint = CollisionUtils.ClosestPointOnSurface( c, position, Radius );
+
+			if (contactPoint != Vector3.zero)
+            {
+            	DebugDraw.DrawMarker(contactPoint, 2.0f, Color.cyan, 0.0f, false);
+
+            	Vector3 v = contactPoint - position;
+            	if( v != Vector3.zero )
+            	{
+            		int layer = c.gameObject.layer;
+            	}
+
+            }
 		}
 	}
 
@@ -68,6 +97,12 @@ public class PlayerController : MonoBehaviour {
 	public void Fall()
 	{
 		moveDirection -= Up * gravity * Time.deltaTime;
+	}
+
+	void OnDrawGizmos()
+	{
+		if( collisionSphere != null )
+			Gizmos.DrawWireSphere (collisionSphere.Position, Radius);
 	}
 }
 
